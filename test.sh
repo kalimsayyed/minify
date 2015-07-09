6009ba02d65acf14006bbdcaa27b62427ff8609c
KALIM <kalimsayyed809@gmail.com>
2015-07-09 15:53:22 +0530

    test

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-for file in `find /home/kalim/git/minify -name "*.js"`
+cd  /home/kalim/git/minify/
+
+for file in `find -name "*.js"`
 do
 echo "Compressing $file …"
 java -jar /home/kalim/min.jar --type js -o "${file%%.*}-min.js" "$file"
 done
 
-for file in `find /home/kalim/git/minify -name "*.css"`
+for file in `find  -name "*.css"`
 do
 echo "Compressing $file …"
 java -jar /home/kalim/min.jar --type css -o "${file%%.*}-min.css" "$file"
 done
 
-sudo find /home/kalim/git/minify/*  -type f -name "*-min.css"  -print0 | xargs -0 sudo chmod 755
-sudo find /home/kalim/git/minify/*  -type f -name "*-min.js"  -print0 | xargs -0 sudo chmod 755
+#sudo find /home/kalim/git/minify/*  -type f -name "*-min.css"  -print0 | xargs -0 sudo chmod 755
+#sudo find /home/kalim/git/minify/*  -type f -name "*-min.js"  -print0 | xargs -0 sudo chmod 755
 
 exit 0
