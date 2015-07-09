3b3d362b8613d6fd055710c886970f11da02ff6c
KALIM <kalimsayyed809@gmail.com>
2015-07-09 16:50:31 +0530

    test

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cd  /home/kalim/git/minify/
+find /home/kalim/git/minify/ -type f  \( -name "*-min.js" -o -name "*-min.css" \)  -exec rm -rf   {} \;
 
 for file in `find /home/kalim/git/minify/ -name "*.js"`
 do
 echo "Compressing $file …"
 java -jar /home/kalim/min.jar --type js -o "${file%%.*}-min.js" "$file"
 done
+
 
 for file in `find /home/kalim/git/minify/ -name "*.css"`
 do
@@ -18,3 +19,4 @@ find /home/kalim/git/minify/*  -type f -name "*-min.css"  -print0 | xargs -0 chm
 find /home/kalim/git/minify/*  -type f -name "*-min.js"  -print0 | xargs -0 chmod 755
 
 exit 0
+
